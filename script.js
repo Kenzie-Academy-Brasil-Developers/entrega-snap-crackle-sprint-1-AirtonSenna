11b90b3cdec5e25ec41fb9c32de20434a47ffa34
Airton Senna <Airtonsenna13@gmail.com>
2021-08-09 18:14:34 -0300

    Sem snapCracklePrime

---
+++ script.js
@@ -0,0 +1,18 @@
+function snapCrackle(maxValue) {
+    let result = '';
+
+    for (let i = 1; i <= maxValue; i++) {
+        
+        if (i % 10 === 0) {
+            result += 'Crackle, '
+        } else if (i % 5 === 0) {
+            result += 'SnapCrackle, '
+        } else if (i % 2 !== 0) {
+            result += 'Snap, '
+        } else {
+            result += i + ', '
+        }
+    }
+    return result
+}
+
